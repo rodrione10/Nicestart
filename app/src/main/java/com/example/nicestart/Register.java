c991a3293d05d429813206861552d26f8555f258
rodrijavi <rodrione1@gmail.com>
2023-11-08 13:38:48 +0100

    otro commit de fotos

--- app/src/main/java/com/example/nicestart/Register.java
+++ app/src/main/java/com/example/nicestart/Register.java
@@ -16,8 +16,8 @@ public class Register extends AppCompatActivity {
     }
     public void openMain(View v){
         Intent intent = new Intent(Register.this, MainActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
     }
 }
