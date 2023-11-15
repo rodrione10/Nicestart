924b1966c39123b603cc1aeb34f3e69e1d08b8c9
rodrijavi <rodrione1@gmail.com>
2023-11-15 13:57:00 +0100

    Splash y blink

--- app/src/main/java/com/example/nicestart/Login.java
+++ app/src/main/java/com/example/nicestart/Login.java
@@ -3,8 +3,13 @@ package com.example.nicestart;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 
 public class Login extends AppCompatActivity {
 
@@ -12,6 +17,14 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ImageView mGirl = findViewById(R.id.girl);
+        Glide.with(this)
+                .load(R.drawable.girl)
+                .transition(DrawableTransitionOptions.withCrossFade(4000))
+                .centerCrop()
+                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.teal_700)))
+                .into(mGirl);
     }
     public void openMain(View v){
         Intent intent = new Intent(Login.this, MainActivity.class);
@@ -23,4 +36,5 @@ public class Login extends AppCompatActivity {
         Intent intent = new Intent (Login.this, Register.class);
         startActivity(intent);
     }
+
 }
